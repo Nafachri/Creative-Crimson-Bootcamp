d83706aba4fb9cfd1d3570adc12b14dbbcb765f4
Naufal Al Fachri <nafa.create@gmail.com>
2020-11-07 16:29:43 +0700

    DOM Exercise-2

--- week-4/day-2/Regex/Exercise-2/Exercise-2.js
+++ week-4/day-2/Regex/Exercise-2/Exercise-2.js
@@ -1,3 +1,22 @@
-// let myRegex = /^\d{10,12}$/g;
-// let hasil = myRegex.test(0123456789012);
-// console.log(hasil);
+let myRegex = /^\d{10,12}$/g;
+let myNumber = Number(prompt("Input Your Phone Number, Min 10 Max 12"))
+let hasil = myRegex.test(myNumber);
+
+if(hasil){
+    console.log("You're number is right")
+    alert("You're number is right")
+}else{
+    console.log("Please input min 10 number & max 12 number");
+    alert("Please input min 10 number & max 12 number");
+}
+
+// DOM 
+
+let userNumber = document.getElementById("user-number")
+
+let myFunction = () => {
+    let tampil = myNumber;
+    userNumber.innerHTML = `Your Phone Number was : ${tampil}`
+}
+myFunction()
+
