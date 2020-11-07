abbc60c49bc6556ac959b617777e3d53526489dd
Naufal Al Fachri <nafa.create@gmail.com>
2020-11-07 16:16:12 +0700

    DOM Exercise-1

--- week-4/day-2/Regex/Exercise-1/Exercise-1.js
+++ week-4/day-2/Regex/Exercise-1/Exercise-1.js
@@ -1,9 +1,22 @@
-let myRegex = /[^A-Z]/;
-let myName = "Naufal Al-FachrI";
+let myRegex = /^[A-Z]/;
+let myName = prompt("Masukan nama anda")
 let hasil = myRegex.test(myName);
 if(hasil){
-    console.log("Huruf ini mengandung huruf besar");
+    console.log("Nama depan anda mengandung huruf besar");
+    alert("Nama depan anda mengandung huruf besar");
 }
 else{
-    console.log("Huruf ini tidak mengandung huruf besar");
+    console.log("Nama depan anda tidak mengandung huruf besar");
+    alert("Nama depan anda tidak mengandung huruf besar");
 }
+
+// DOM 
+// akses id paragraf di html, setiap kali user input nama maka tampilkan di sini
+
+let namaUser = document.getElementById("nama-user")
+
+let myFunction = () => {
+    let tampil = myName;
+    namaUser.innerText = `Selamat Datang ${tampil} !`;
+};
+myFunction()
