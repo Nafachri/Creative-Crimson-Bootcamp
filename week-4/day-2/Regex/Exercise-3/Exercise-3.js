522c4b7bb7ab09de3e34ebb986995d2d3b9364e6
Naufal Al Fachri <nafa.create@gmail.com>
2020-11-08 12:54:22 +0700

    DOM Exercise-3

--- week-4/day-2/Regex/Exercise-3/Exercise-3.js
+++ week-4/day-2/Regex/Exercise-3/Exercise-3.js
@@ -1,21 +1,42 @@
-let myRegex = /^Bagaimana Cara Memulai Usaha Bisnis|Bootcamp Impact Byte|Status Covid Hari Ini$/
-let tanyaUser = prompt(`Pilih 1 Topik : ${myRegex}`)
-let hasil = myRegex.test(tanyaUser)
-console.log(hasil);
-
-if(hasil){
-    alert(`Topik yang anda pilih adalah ${tanyaUser}`)
-}
-else{
-    prompt(`Pilih Topik Yang Tersedia : ${myRegex}`)
-}
-
-//DOM 
-
-outputUser = document.getElementById("output-user")
+let inputTopicUser = document.getElementById("input-topic-user");
+let buttonClick = document.getElementById("button-click");
+let outputTopicUser = document.getElementById("output-topic-user");
 
 let myFunction = () => {
-    let tampil = tanyaUser;
-    outputUser.innerHTML = tampil;
+    let myRegex = /^Bagaimana Cara Memulai Usaha Bisnis|Bootcamp Impact Byte|Status Covid Hari Ini$/
+    let userTopic = inputTopicUser.value;
+    let test = myRegex.test(userTopic);
+
+    if(test){
+        outputTopicUser.innerHTML = `Topic Yang Anda Pilih Adalah : ${userTopic}`
+        console.log(userTopic);
+    }else{
+        console.log("User Tidak Memilih Topik Yang Ada");
+    }
+    return false;
 }
-myFunction()
+buttonClick.onclick = myFunction();
+
+
+
+// let myRegex = /^Bagaimana Cara Memulai Usaha Bisnis|Bootcamp Impact Byte|Status Covid Hari Ini$/
+// let tanyaUser = prompt(`Pilih 1 Topik : ${myRegex}`)
+// let hasil = myRegex.test(tanyaUser)
+// console.log(hasil);
+
+// if(hasil){
+//     alert(`Topik yang anda pilih adalah ${tanyaUser}`)
+// }
+// else{
+//     prompt(`Pilih Topik Yang Tersedia : ${myRegex}`)
+// }
+
+// //DOM 
+
+// outputUser = document.getElementById("output-user")
+
+// let myFunction = () => {
+//     let tampil = tanyaUser;
+//     outputUser.innerHTML = tampil;
+// }
+// myFunction()
