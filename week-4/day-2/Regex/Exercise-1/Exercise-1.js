e2b6cc711f5479b42295bb65c24f8a8a42e86bfb
Naufal Al Fachri <nafa.create@gmail.com>
2020-11-08 12:15:49 +0700

    DOM Exercise-1

--- week-4/day-2/Regex/Exercise-1/Exercise-1.js
+++ week-4/day-2/Regex/Exercise-1/Exercise-1.js
@@ -1,22 +1,21 @@
-let myRegex = /^[A-Z]/;
-let myName = prompt("Masukan nama anda")
-let hasil = myRegex.test(myName);
-if(hasil){
-    console.log("Nama depan anda mengandung huruf besar");
-    alert("Nama depan anda mengandung huruf besar");
-}
-else{
-    console.log("Nama depan anda tidak mengandung huruf besar");
-    alert("Nama depan anda tidak mengandung huruf besar");
-}
+let buttonClick = document.getElementById("button-click");
+let inputNamaUser = document.getElementById("input-nama-user");
+let outputNameUser = document.getElementById("output-nama-user");
 
-// DOM 
-// akses id paragraf di html, setiap kali user input nama maka tampilkan di sini
-
-let namaUser = document.getElementById("nama-user")
 
 let myFunction = () => {
-    let tampil = myName;
-    namaUser.innerText = `Selamat Datang ${tampil} !`;
-};
-myFunction()
+    let myRegex = /^[A-Z]/
+    let username = inputNamaUser.value;
+    let test = myRegex.test(username);
+
+    if(test){
+        outputNameUser.innerHTML = `Selamat Datang ${username} !`
+        console.log("Huruf Depan Nama Mengandung Huruf Besar");
+    }else{
+        outputNameUser.innerHTML = `Selamat Datang ${username} !`
+        console.log("Huruf Depan Nama Tidak Mengandung Huruf Depan");
+        
+    }
+    return false;
+}
+buttonClick.onclick = myFunction;
