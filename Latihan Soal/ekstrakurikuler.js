326757a590bf2b652504bb68e55fe455b30703fc
Naufal Al Fachri <nafa.create@gmail.com>
2020-11-12 14:25:01 +0700

    estrakurikuler masih bingung

---
+++ Latihan Soal/ekstrakurikuler.js
@@ -0,0 +1,83 @@
+/* 
+  Pada sebuah sekolah, terdapat banyak club ekstrakurikuler. Setiap siswa wajib untuk mengikuti minimal 1 club ekstrakulikuler tersebut.
+  Sekolah ini membutuhkan sebuah aplikasi yang dapat mengelompokan data berdasarkan nama club yang di ikuti oleh siswa beserta kelasnya.
+
+  [instruction]
+  1. Buatlah sebuah fungsi untuk memproses data sehingga bisa mengeluarkan output seperti dengan sample. Untuk fungsi pertama ini hanya
+   boleh menggunakan looping for, dilarang menggunakan build in function javascript (.filter(), .map(), .reduce(), .includes(), .indexOf() . forEach, dan lain-lain)
+
+  2. Buatlah sebuah fungsi yang melakukan logika yang sama dengan fungsi di atas, namun mengggunakan salah satu atau kombinasi build in
+   function javascript (.filter(), .map(), .reduce(), .includes(), .indexOf(), dan lain-lain)
+*/
+
+let data = 
+[
+  {
+    name: 'Adi',
+    class: 'Mawar',
+    club: ['Bola', 'Bulutangkis']
+  },
+  {
+    name: 'Cinthya',
+    class: 'Melati',
+    club: ['Membaca', 'Bulutangkis']
+  },
+  {
+    name: 'Andika',
+    class: 'Mawar',
+    club: ['Bulutangkis', 'Membaca']
+  },
+  {
+    name: 'Daniel',
+    class: 'Mawar',
+    club: ['Bola', 'Renang']
+  }
+]
+
+let getData = () => {
+  for(let i = 0; i < data.length; i++){
+    console.log(data[i]);
+  }
+}
+getData()
+
+let eskul = () => {
+  for(let j = 0; j < data.length; j ++){
+    if(data[j].club.includes("Bulutangkis")){
+      console.log(data[j])
+    }
+  }
+}
+
+eskul()
+
+
+
+
+
+
+// function ekstrakurikuler(data){
+//   for(let i = 0; i < data.length; i++){
+//     console
+//   }
+// }
+// ​
+// console.log(ekstrakurikuler(data));
+
+/* example output:
+{
+  Bola: {
+    Mawar: ['Adi', 'Daniel']
+  },
+  Bulutangkis: {
+    Mawar: ['Adi', 'Andika'],
+    Melati: ['Cinthya']
+  },
+  Membaca: {
+    Melati: ['Cinthya'],
+    Mawar: ['Andika']
+  },
+  Renang: {
+    Mawar: ['Daniel']
+  }
+} */
