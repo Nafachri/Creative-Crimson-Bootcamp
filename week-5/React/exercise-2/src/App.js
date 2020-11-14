fb332f08fa0e91017d371ed47d427b05b92ee835
Naufal Al Fachri <nafa.create@gmail.com>
2020-11-14 23:21:06 +0700

    create new folder called ListProduct

--- week-5/React/exercise-2/src/App.js
+++ week-5/React/exercise-2/src/App.js
@@ -1,57 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react' 
+import './components/ListProduct';
+import React from 'react' 
+import ListProduct from './components/ListProduct';
 
-function ListProduct() {
-
-  const [arrBuah, setArrBuah] = useState([
-    {buah: "Semangka", harga: "10.000"},
-    {buah: "Nanas", harga: "5.000"},
-    {buah: "Jeruk", harga: "12.000"},
-    {buah: "Kedondong", harga: "10.000"},
-    {buah: "Duku Palembang", harga: "11.000"}
-  ])
+function App(){
   return(
     <div>
-      <Product 
-      buah={arrBuah[0].buah}
-      harga={arrBuah[0].harga}
-      />
-      <Product 
-      buah={arrBuah[1].buah}
-      harga={arrBuah[1].harga}
-      />
-      <Product 
-      buah={arrBuah[2].buah}
-      harga={arrBuah[2].harga}
-      />
-      <Product 
-      buah={arrBuah[3].buah}
-      harga={arrBuah[3].harga}
-      />
-      <Product 
-      buah={arrBuah[4].buah}
-      harga={arrBuah[4].harga}
-      />
+      <ListProduct />
     </div>
   )
 }
 
-function Product(props){
-  return(
-    <>
-    <section className="toko-djaja">
-      <h1>Tojo Djaja</h1>
-      <table>
-        <tr>
-        <th>{props.buah}</th>
-        </tr>
-        <tr>
-          <td>{props.harga}</td>
-        </tr>
-      </table>
-    </section>
-    </>
-  )
-}
-export default ListProduct;
+export default App;
