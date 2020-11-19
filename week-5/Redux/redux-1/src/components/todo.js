b21ed96e833db6d51745a9bf199bf0ea584c1da4
Naufal Al Fachri <nafa.create@gmail.com>
2020-11-19 15:47:15 +0700

    delete

--- week-5/Redux/redux-1/src/components/todo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Todo() {
-    return(
-        <div className="todo-item">
-            <span>1.</span> <span>Belajar Redux</span>
-        </div>
-    )
-}
--- week-5/Redux/redux-1/src/index.js
+++ week-5/Redux/redux-1/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TodoApp from './todoApp';
-import './style.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoApp />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
